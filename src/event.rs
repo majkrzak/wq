d7f7c19d20ea275fe7e56fd32c719444aa86a83a
Piotr Majkrzak <piotr@majkrzak.dev>
2025-11-26 17:22:41 +0100

    Rework event model

---
+++ src/event.rs
@@ -0,0 +1,36 @@
+use native_db::{Key, ToKey, native_db};
+use native_model::{Model, native_model};
+use serde::{Deserialize, Serialize};
+use ulid::Ulid;
+use ulid::serde::ulid_as_u128;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone, Copy)]
+pub struct EventID(#[serde(with = "ulid_as_u128")] Ulid);
+
+impl ToKey for EventID {
+    fn to_key(&self) -> Key {
+        self.0.0.to_key()
+    }
+    fn key_names() -> Vec<String> {
+        vec!["EventID".to_string()]
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[native_model(id = 1, version = 1)]
+#[native_db]
+pub struct Event {
+    #[primary_key]
+    id: EventID,
+    kind: EventKind,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum EventKind {
+    Enqueue,
+    Dequeue,
+    Start,
+    Stop,
+    Link(String),
+    Comment(String),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-pub mod model;
+use std::sync::LazyLock;
+
+use native_db::Models;
+
+use crate::event::Event;
+
+pub mod event;
+
+pub static MODELS: LazyLock<Models> = LazyLock::new(|| {
+    let mut models = Models::new();
+    models.define::<Event>().unwrap();
+    models
+});
